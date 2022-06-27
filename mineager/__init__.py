1f067227284c74d8d7278c92b0b9078f3ae3a2ac
ooliver1 <oliverwilkes2006@icloud.com>
2022-06-27 19:28:21 +0100

    feat: add package base

---
+++ mineager/__init__.py
@@ -0,0 +1,10 @@
+# SPDX-License-Identifier: MIT
+
+import logging as __logging
+
+__version__ = "0.0.0"
+__author__ = "ooliver1"
+__license__ = "MIT"
+__title__ = "mineager"
+
+__logging.getLogger(__name__).addHandler(__logging.NullHandler())
