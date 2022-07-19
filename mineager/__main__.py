777ba7a4a81be74861813b864b2b7720b0637b7b
ooliver1 <oliverwilkes2006@icloud.com>
2022-07-19 22:48:35 +0100

    feat: add __main__.py

---
+++ mineager/__main__.py
@@ -0,0 +1,20 @@
+# SPDX-License-Identifier: MIT
+
+from asyncio import get_event_loop
+from asyncio import run as async_run
+from signal import SIGTERM
+
+from mineager import run
+
+
+async def main():
+    loop = get_event_loop()
+
+    stop = loop.create_future()
+    loop.add_signal_handler(SIGTERM, stop.set_result, None)
+
+    async with run():
+        await stop
+
+
+async_run(main())
